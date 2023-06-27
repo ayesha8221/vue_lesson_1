e55c8f1cd00fbdf6a80c0b1e58b214a1f520f00b
ayesha8221 <ayesha.glnt012@gmail.com>
2023-06-27 16:40:02 +0200

    lesson_1

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <!-- <nav>
+import FooterComp from './components/FooterComp.vue';
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/> -->
+  <NavbarComp/>
+  <router-view/>
+  <FooterComp/>
+</template>
+<script>
+import NavbarComp from './components/NavbarComp.vue';
+import FooterComp from './components/FooterComp.vue';
+
+export default {
+
+  components: { NavbarComp, FooterComp }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #365e86;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #4e6f91;
+}
+
+nav a.router-link-exact-active {
+  color: #4bb284;
+  display: flex;
+  justify-content: start;
+  /* align-items: flex-start; */
+}
+</style>
---
+++ src/components/FooterComp.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="footer">
+        <h6>Footer</h6>
+        <p class="footer-text-left">
+            <router-link class="menu" to="/">Home</router-link>
+            <router-link class="menu" to="/">Tutorial</router-link>
+            <router-link class="menu" to="/">About</router-link>
+        </p>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+.footer {
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    height: 90px;
+    background-color: chocolate;
+}
+
+.footer-text-left {
+    font-size: 25px;
+    padding-left: 40px;
+    float: left;
+    word-spacing: 20px;
+}
+
+.menu:hover {
+    background-color: yellowgreen;
+    font-size: 20px;
+}
+</style>
